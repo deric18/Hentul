4d79b6776bc1abd8a975528b53b65aee5d90a83e
Deric Pinto <depint@microsoft.com>
2024-04-05 17:26:53 -0700

    Logic for SOM

--- SecondOrderMemory/BehaviourManagers/SOMBlockManager.cs
+++ SecondOrderMemory/BehaviourManagers/SOMBlockManager.cs
@@ -46,8 +46,22 @@ namespace SecondOrderMemory.BehaviourManagers
         private void EstablishCurrentState()
         {
             //Check if there is consensus amoung all the blocks either by unioun and Intersection and update CurrentState Enum 
+            
+            //SOM cannot uniquely identify an object unless it has previously identified a unique pattern which has linked succesfully to an Object (Higher Order Pattern) ,
+            //There has to be some level of Supervised learning here , where 
+            //The network has to be trained to identify the object as an apple or orange or house or car as such ...
+            // Build the SOM Layer first to see how it works the nfrom the data we can build the logic for higher predictive feedback Layers.
 
-            //Question : what if there is dissonance amoung the blocks ?
+
+            //AlgorithM : 
+
+            //Step 1: Keep checking with all the layers for Confident Classification
+            //Step 2: If one of the blocks return abck with a Confident Classification -> cross check this classification with all the other layers , if all of them agree Classify the input to corresponding Object.
+            //Step 3: If 2 different block respond with 2 different Classifications with high degree of Confidence , This is a misconfigured Network , Needs More Training , One of the is wrong or both of the mare wrong. Needs Investigation Step 6.
+            //Step 4: If 1 Block votes for one object and the rest of the blocks vote for another object, Needs Investigation Step 6.
+            //Step 5: All the Blocks are still Confused , Keep repeating Step 1.
+            //Step 6: Investigate Dissonance in the network!
+
 
 
 
