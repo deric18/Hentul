1817875f94998971380260cbbcd00584e78a05a9
Deric Pinto <depint@microsoft.com>
2024-08-10 18:20:43 -0700

    removed unused usings on Baseobject.cs

--- Hentul/Hippocampal_Entorinal_complex/BaseObject.cs
+++ Hentul/Hippocampal_Entorinal_complex/BaseObject.cs
@@ -1,13 +1,7 @@
 ﻿/// Author : Deric Pinto
 
 namespace Hentul.Hippocampal_Entorinal_complex
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
+{    
 
     public class BaseObject
     {
